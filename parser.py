2a83239942c7782b78ac5cd497b75fce6e202363
joshstenis <joshstenis@gmail.com>
2021-05-13 20:32:07 -0500

    Try is created via tuples (a la calculator example)

--- parser.py
+++ parser.py
@@ -95,46 +95,53 @@ l = lex.lex()
 # ---------------------------------------------
 
 # Takes in 2 a_expr's and an a_op, then return result of the operation
-def getResult(op, e1, e2):
-    if op == 'MUL':
-        return e1.value * e2.value
-    elif op == 'DIV':
-        return e1.value / e2.value
-    elif op == 'ADD':
-        return e1.value + e2.value
-    elif op == 'SUB':
-        return e1.value - e2.value
-    else: return None
+# def getResult(op, e1, e2):
+#     if op == 'MUL':
+#         return e1.value * e2.value
+#     elif op == 'DIV':
+#         return e1.value / e2.value
+#     elif op == 'ADD':
+#         return e1.value + e2.value
+#     elif op == 'SUB':
+#         return e1.value - e2.value
+#     else: return None
+
+
+import re
 
 def p_program(p):
     '''program : stmt_list SEMICOLON'''
+    print('{} ;'.format(p[1]))
 
 def p_stmt_list(p):
     '''stmt_list : stmt_list SEMICOLON stmt 
         | stmt'''
+    try:
+        p[0] = (p[1], ';', p[3])
+    except IndexError:
+        p[0] = p[1]
 
 def p_stmt(p):
     '''stmt : assignment 
         | read 
         | write 
         | declaration'''
+    p[0] = p[1]
 
 def p_assignment(p):
     '''assignment : varref ASSIGN a_expr'''
-    if p[3] == 'FLOAT':
-        p[0] = float(p[3])
-    elif p[3] == 'INTEGER':
-        p[0] = int(p[3])
+    p[0] = (':=', p[1], p[3])
 
 def p_declaration(p):
     '''declaration : datatype ID'''
+    p[0] = (p[1], p[2])
     
     # =======- CHECK ME -=======
     
-    if p[1] == 'DT_INT':
-        p[0] = int(p[2])
-    elif p[1] == 'DT_FLOAT':
-        p[0] = float(p[2])
+    # if p[1] == 'DT_INT':
+    #     p[0] = int(p[2])
+    # elif p[1] == 'DT_FLOAT':
+    #     p[0] = float(p[2])
 
 def p_datatype(p):
     '''datatype : DT_INT 
@@ -148,23 +155,30 @@ def p_a_expr(p):
               | FLOAT
               | varref
               | LPAREN a_expr RPAREN'''
+    try:
+        if re.compile(r'\+|-|\*|/').match(p[2]): p[0] = (p[2], p[1], p[3])
+    except IndexError:
+        if p[1] == 'SUB': p[0] = ('NEGATIVE', p[2])
+        elif p[1] == 'LPAREN': p[0] = ('(', p[2], ')')
+        else: p[0] = p[1]
+    
 
     # =======- CHECK ME -=======
 
-    if p[1] == 'SUB':
-        p[0] = -1 * p[2]
-    elif p[1] == 'INTEGER':
-        if p[2] is None:
-            p[0] = int(p[1])
-        elif p[2] == 'a_op' and p[3] == 'FLOAT':
-            p[0] = getResult(p[2], float(p[1]),  float(p[3]))
-        else:
-            p[0] = getResult(p[2], int(p[1]),  int(p[3]))
-    elif p[1] == 'FLOAT':
-        if p[2] is None:
-            p[0] = float(p[1])
-        elif p[2] == 'a_op':
-            p[0] = getResult(p[2], float(p[1]),  float(p[3]))
+    # if p[1] == 'SUB':
+    #     p[0] = -1 * p[2]
+    # elif p[1] == 'INTEGER':
+    #     if p[2] is None:
+    #         p[0] = int(p[1])
+    #     elif p[2] == 'a_op' and p[3] == 'FLOAT':
+    #         p[0] = getResult(p[2], float(p[1]),  float(p[3]))
+    #     else:
+    #         p[0] = getResult(p[2], int(p[1]),  int(p[3]))
+    # elif p[1] == 'FLOAT':
+    #     if p[2] is None:
+    #         p[0] = float(p[1])
+    #     elif p[2] == 'a_op':
+    #         p[0] = getResult(p[2], float(p[1]),  float(p[3]))
 
 def p_a_op(p):
     '''a_op : ADD 
@@ -175,24 +189,35 @@ def p_a_op(p):
 
 def p_varref(p):
     '''varref : ID'''
+    p[0] = p[1]
     # ACTION: find associated variable (see symtab.cc)
 
 def p_read(p):
     '''read : READ varlist'''
+    p[0] = (p[1], p[2])
     # ACTION: iterate through varref objects (see OP_READ)
 
 def p_write(p):
     '''write : WRITE expr_list'''
+    p[0] = (p[1], p[2])
     # ACTION: write given variable to symbol_t objects (see OP_WRITE)
 
 def p_varlist(p):
     '''varlist : varlist COMMA varref 
         | varref'''
+    try:
+        p[0] = (p[1], p[3])
+    except IndexError:
+        p[0] = p[1]
     # ACTION: stack manipulation, not sure if python needs this (see grammar.y)
 
 def p_expr_list(p):
     '''expr_list : expr_list COMMA a_expr 
         | a_expr'''
+    try:
+        p[0] = (p[1], p[3])
+    except IndexError:
+        p[0] = p[1]
     # ACTION: stack manipulation, not sure if python needs this (see grammar.y)
 
 def p_error(p):
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'ADD ASSIGN COLON COMMA COMMENT DIV DT_FLOAT DT_INT FLOAT ID INTEGER LBRACK LITERAL_STR LPAREN MUL RBRACK READ RPAREN SEMICOLON SUB WRITEprogram : stmt_list SEMICOLONstmt_list : stmt_list SEMICOLON stmt \n        | stmtstmt : assignment \n        | read \n        | write \n        | declarationassignment : varref ASSIGN a_exprdeclaration : datatype IDdatatype : DT_INT \n        | DT_FLOATa_expr : a_expr ADD a_term \n        | a_expr SUB a_term \n        | a_terma_term : a_term MUL a_fact \n        | a_term DIV a_fact \n        | a_facta_fact : varref \n        | INTEGER \n        | FLOAT \n        | LPAREN a_expr RPAREN \n        | SUB a_fact\n        | LITERAL_STRvarref : IDread : READ varlistwrite : WRITE expr_listvarlist : varlist COMMA varref \n        | varrefexpr_list : expr_list COMMA a_expr \n        | a_expr'
+_lr_signature = 'ADD ASSIGN COLON COMMA COMMENT DIV DT_FLOAT DT_INT FLOAT ID INTEGER LBRACK LITERAL_STR LPAREN MUL RBRACK READ RPAREN SEMICOLON SUB WRITEprogram : stmt_list SEMICOLONstmt_list : stmt_list SEMICOLON stmt \n        | stmtstmt : assignment \n        | read \n        | write \n        | declarationassignment : varref ASSIGN a_exprdeclaration : datatype IDdatatype : DT_INT \n        | DT_FLOATa_expr : a_expr a_op a_expr\n              | SUB a_expr \n              | INTEGER\n              | FLOAT\n              | varref\n              | LPAREN a_expr RPARENa_op : ADD \n            | SUB \n            | MUL \n            | DIVvarref : IDread : READ varlistwrite : WRITE expr_listvarlist : varlist COMMA varref \n        | varrefexpr_list : expr_list COMMA a_expr \n        | a_expr'
     
-_lr_action_items = {'READ':([0,15,],[9,9,]),'WRITE':([0,15,],[10,10,]),'ID':([0,9,10,11,13,14,15,16,22,27,32,33,34,35,36,37,],[12,12,12,29,-10,-11,12,12,12,12,12,12,12,12,12,12,]),'DT_INT':([0,15,],[13,13,]),'DT_FLOAT':([0,15,],[14,14,]),'$end':([1,15,],[0,-1,]),'SEMICOLON':([2,3,4,5,6,7,12,17,18,19,20,21,23,24,25,26,28,29,30,31,38,40,41,42,43,44,45,46,],[15,-3,-4,-5,-6,-7,-24,-25,-28,-26,-30,-14,-17,-18,-19,-20,-23,-9,-2,-8,-22,-27,-29,-12,-13,-15,-16,-21,]),'ASSIGN':([8,12,],[16,-24,]),'INTEGER':([10,16,22,27,33,34,35,36,37,],[25,25,25,25,25,25,25,25,25,]),'FLOAT':([10,16,22,27,33,34,35,36,37,],[26,26,26,26,26,26,26,26,26,]),'LPAREN':([10,16,22,27,33,34,35,36,37,],[27,27,27,27,27,27,27,27,27,]),'SUB':([10,12,16,20,21,22,23,24,25,26,27,28,31,33,34,35,36,37,38,39,41,42,43,44,45,46,],[22,-24,22,35,-14,22,-17,-18,-19,-20,22,-23,35,22,22,22,22,22,-22,35,35,-12,-13,-15,-16,-21,]),'LITERAL_STR':([10,16,22,27,33,34,35,36,37,],[28,28,28,28,28,28,28,28,28,]),'COMMA':([12,17,18,19,20,21,23,24,25,26,28,38,40,41,42,43,44,45,46,],[-24,32,-28,33,-30,-14,-17,-18,-19,-20,-23,-22,-27,-29,-12,-13,-15,-16,-21,]),'MUL':([12,21,23,24,25,26,28,38,42,43,44,45,46,],[-24,36,-17,-18,-19,-20,-23,-22,36,36,-15,-16,-21,]),'DIV':([12,21,23,24,25,26,28,38,42,43,44,45,46,],[-24,37,-17,-18,-19,-20,-23,-22,37,37,-15,-16,-21,]),'ADD':([12,20,21,23,24,25,26,28,31,38,39,41,42,43,44,45,46,],[-24,34,-14,-17,-18,-19,-20,-23,34,-22,34,34,-12,-13,-15,-16,-21,]),'RPAREN':([12,21,23,24,25,26,28,38,39,42,43,44,45,46,],[-24,-14,-17,-18,-19,-20,-23,-22,46,-12,-13,-15,-16,-21,]),}
+_lr_action_items = {'READ':([0,15,],[9,9,]),'WRITE':([0,15,],[10,10,]),'ID':([0,9,10,11,13,14,15,16,21,25,29,30,31,32,33,34,35,],[12,12,12,26,-10,-11,12,12,12,12,12,12,12,-18,-19,-20,-21,]),'DT_INT':([0,15,],[13,13,]),'DT_FLOAT':([0,15,],[14,14,]),'$end':([1,15,],[0,-1,]),'SEMICOLON':([2,3,4,5,6,7,12,17,18,19,20,22,23,24,26,27,28,36,38,39,40,41,],[15,-3,-4,-5,-6,-7,-22,-23,-26,-24,-28,-14,-15,-16,-9,-2,-8,-13,-25,-27,-12,-17,]),'ASSIGN':([8,12,],[16,-22,]),'SUB':([10,12,16,20,21,22,23,24,25,28,30,31,32,33,34,35,36,37,39,40,41,],[21,-22,21,33,21,-14,-15,-16,21,33,21,21,-18,-19,-20,-21,33,33,33,33,-17,]),'INTEGER':([10,16,21,25,30,31,32,33,34,35,],[22,22,22,22,22,22,-18,-19,-20,-21,]),'FLOAT':([10,16,21,25,30,31,32,33,34,35,],[23,23,23,23,23,23,-18,-19,-20,-21,]),'LPAREN':([10,16,21,25,30,31,32,33,34,35,],[25,25,25,25,25,25,-18,-19,-20,-21,]),'COMMA':([12,17,18,19,20,22,23,24,36,38,39,40,41,],[-22,29,-26,30,-28,-14,-15,-16,-13,-25,-27,-12,-17,]),'ADD':([12,20,22,23,24,28,36,37,39,40,41,],[-22,32,-14,-15,-16,32,32,32,32,32,-17,]),'MUL':([12,20,22,23,24,28,36,37,39,40,41,],[-22,34,-14,-15,-16,34,34,34,34,34,-17,]),'DIV':([12,20,22,23,24,28,36,37,39,40,41,],[-22,35,-14,-15,-16,35,35,35,35,35,-17,]),'RPAREN':([12,22,23,24,36,37,40,41,],[-22,-14,-15,-16,-13,41,-12,-17,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'program':([0,],[1,]),'stmt_list':([0,],[2,]),'stmt':([0,15,],[3,30,]),'assignment':([0,15,],[4,4,]),'read':([0,15,],[5,5,]),'write':([0,15,],[6,6,]),'declaration':([0,15,],[7,7,]),'varref':([0,9,10,15,16,22,27,32,33,34,35,36,37,],[8,18,24,8,24,24,24,40,24,24,24,24,24,]),'datatype':([0,15,],[11,11,]),'varlist':([9,],[17,]),'expr_list':([10,],[19,]),'a_expr':([10,16,27,33,],[20,31,39,41,]),'a_term':([10,16,27,33,34,35,],[21,21,21,21,42,43,]),'a_fact':([10,16,22,27,33,34,35,36,37,],[23,23,38,23,23,23,23,44,45,]),}
+_lr_goto_items = {'program':([0,],[1,]),'stmt_list':([0,],[2,]),'stmt':([0,15,],[3,27,]),'assignment':([0,15,],[4,4,]),'read':([0,15,],[5,5,]),'write':([0,15,],[6,6,]),'declaration':([0,15,],[7,7,]),'varref':([0,9,10,15,16,21,25,29,30,31,],[8,18,24,8,24,24,24,38,24,24,]),'datatype':([0,15,],[11,11,]),'varlist':([9,],[17,]),'expr_list':([10,],[19,]),'a_expr':([10,16,21,25,30,31,],[20,28,36,37,39,40,]),'a_op':([20,28,36,37,39,40,],[31,31,31,31,31,31,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,34 +27,32 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> program","S'",1,None,None,None),
-  ('program -> stmt_list SEMICOLON','program',2,'p_program','parser.py',58),
-  ('stmt_list -> stmt_list SEMICOLON stmt','stmt_list',3,'p_stmt_list','parser.py',61),
-  ('stmt_list -> stmt','stmt_list',1,'p_stmt_list','parser.py',62),
-  ('stmt -> assignment','stmt',1,'p_stmt','parser.py',65),
-  ('stmt -> read','stmt',1,'p_stmt','parser.py',66),
-  ('stmt -> write','stmt',1,'p_stmt','parser.py',67),
-  ('stmt -> declaration','stmt',1,'p_stmt','parser.py',68),
-  ('assignment -> varref ASSIGN a_expr','assignment',3,'p_assignment','parser.py',71),
-  ('declaration -> datatype ID','declaration',2,'p_declaration','parser.py',75),
-  ('datatype -> DT_INT','datatype',1,'p_datatype','parser.py',79),
-  ('datatype -> DT_FLOAT','datatype',1,'p_datatype','parser.py',80),
-  ('a_expr -> a_expr ADD a_term','a_expr',3,'p_a_expr','parser.py',84),
-  ('a_expr -> a_expr SUB a_term','a_expr',3,'p_a_expr','parser.py',85),
-  ('a_expr -> a_term','a_expr',1,'p_a_expr','parser.py',86),
-  ('a_term -> a_term MUL a_fact','a_term',3,'p_a_term','parser.py',90),
-  ('a_term -> a_term DIV a_fact','a_term',3,'p_a_term','parser.py',91),
-  ('a_term -> a_fact','a_term',1,'p_a_term','parser.py',92),
-  ('a_fact -> varref','a_fact',1,'p_a_fact','parser.py',96),
-  ('a_fact -> INTEGER','a_fact',1,'p_a_fact','parser.py',97),
-  ('a_fact -> FLOAT','a_fact',1,'p_a_fact','parser.py',98),
-  ('a_fact -> LPAREN a_expr RPAREN','a_fact',3,'p_a_fact','parser.py',99),
-  ('a_fact -> SUB a_fact','a_fact',2,'p_a_fact','parser.py',100),
-  ('a_fact -> LITERAL_STR','a_fact',1,'p_a_fact','parser.py',101),
-  ('varref -> ID','varref',1,'p_varref','parser.py',105),
-  ('read -> READ varlist','read',2,'p_read','parser.py',109),
-  ('write -> WRITE expr_list','write',2,'p_write','parser.py',113),
-  ('varlist -> varlist COMMA varref','varlist',3,'p_varlist','parser.py',117),
-  ('varlist -> varref','varlist',1,'p_varlist','parser.py',118),
-  ('expr_list -> expr_list COMMA a_expr','expr_list',3,'p_expr_list','parser.py',122),
-  ('expr_list -> a_expr','expr_list',1,'p_expr_list','parser.py',123),
+  ('program -> stmt_list SEMICOLON','program',2,'p_program','parser.py',110),
+  ('stmt_list -> stmt_list SEMICOLON stmt','stmt_list',3,'p_stmt_list','parser.py',114),
+  ('stmt_list -> stmt','stmt_list',1,'p_stmt_list','parser.py',115),
+  ('stmt -> assignment','stmt',1,'p_stmt','parser.py',120),
+  ('stmt -> read','stmt',1,'p_stmt','parser.py',121),
+  ('stmt -> write','stmt',1,'p_stmt','parser.py',122),
+  ('stmt -> declaration','stmt',1,'p_stmt','parser.py',123),
+  ('assignment -> varref ASSIGN a_expr','assignment',3,'p_assignment','parser.py',127),
+  ('declaration -> datatype ID','declaration',2,'p_declaration','parser.py',131),
+  ('datatype -> DT_INT','datatype',1,'p_datatype','parser.py',142),
+  ('datatype -> DT_FLOAT','datatype',1,'p_datatype','parser.py',143),
+  ('a_expr -> a_expr a_op a_expr','a_expr',3,'p_a_expr','parser.py',147),
+  ('a_expr -> SUB a_expr','a_expr',2,'p_a_expr','parser.py',148),
+  ('a_expr -> INTEGER','a_expr',1,'p_a_expr','parser.py',149),
+  ('a_expr -> FLOAT','a_expr',1,'p_a_expr','parser.py',150),
+  ('a_expr -> varref','a_expr',1,'p_a_expr','parser.py',151),
+  ('a_expr -> LPAREN a_expr RPAREN','a_expr',3,'p_a_expr','parser.py',152),
+  ('a_op -> ADD','a_op',1,'p_a_op','parser.py',177),
+  ('a_op -> SUB','a_op',1,'p_a_op','parser.py',178),
+  ('a_op -> MUL','a_op',1,'p_a_op','parser.py',179),
+  ('a_op -> DIV','a_op',1,'p_a_op','parser.py',180),
+  ('varref -> ID','varref',1,'p_varref','parser.py',184),
+  ('read -> READ varlist','read',2,'p_read','parser.py',189),
+  ('write -> WRITE expr_list','write',2,'p_write','parser.py',194),
+  ('varlist -> varlist COMMA varref','varlist',3,'p_varlist','parser.py',199),
+  ('varlist -> varref','varlist',1,'p_varlist','parser.py',200),
+  ('expr_list -> expr_list COMMA a_expr','expr_list',3,'p_expr_list','parser.py',206),
+  ('expr_list -> a_expr','expr_list',1,'p_expr_list','parser.py',207),
 ]
